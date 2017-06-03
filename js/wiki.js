e9d8cbe88f3f1730590ef5389be931963360727d
Nicholas Galarza <nicogala93@gmail.com>
2017-06-02 20:41:05 -0400

    finally fixed the cors issue with jsonp and callback=?

--- js/wiki.js
+++ js/wiki.js
@@ -2,27 +2,21 @@
 $(document).ready(function() {
 
     $('#submit').on("click", function() {
-      var input = $('#query').val();
-      console.log(input);
+        var keyword = $('#query').val();
+        console.log(keyword);
+
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?";
+
+        $.ajax({
+            dataType: "jsonp",
+            type: "GET",
+            url: url,
+            async: true,
+            success: function(data) {
+                console.log(data);
+            }
+        });
     })
 
-    /*
-    $.ajax({
-        url: "https://en.wikipedia.org/w/api.php",
-        data: {
-            action: 'query',
-            list: 'search',
-            srsearch: $("input[name=query]").val(),
-            format: 'json'
-        },
-        dataType: 'jsonp',
-        type: 'POST',
-        headers: {
-            'Api-User-Agent': 'Example/1.0'
-        },
-        success: function(data) {
-            console.log(data);
-        }
-    });
-    */
+
 });
