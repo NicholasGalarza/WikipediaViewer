962953808b71fdf55ac8b358fe8c3236d99917f2
Nicholas Galarza <nicogala93@gmail.com>
2017-06-03 19:17:53 -0400

    Added JQuery, API now returns results, and the style is bland.

--- js/wiki.js
+++ js/wiki.js
@@ -1,11 +1,9 @@
 'use strict'
 $(document).ready(function() {
 
-    $('#submit').on("click", function() {
-        var keyword = $('#query').val();
-        console.log(keyword);
-
-        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?";
+    $('#submit').click(function() {
+        var keyword = $('#query').val(),
+            url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?";
 
         $.ajax({
             dataType: "jsonp",
@@ -13,7 +11,26 @@ $(document).ready(function() {
             url: url,
             async: true,
             success: function(data) {
-                console.log(data);
+                var searched = data[0],
+                    title = data[1],
+                    description = data[2],
+                    link = data[3],
+                    results = [];
+
+                for (var i = 0; i < title.length; i++) {
+                    if (description[i] === "") {
+                        description[i] = "No description available for this article.";
+                    }
+
+                    results.push('<a href="' + link[i] + '">' +
+                        '<div class="entry">' +
+                        '<h3 class="title">' + title[i] + '</h3>' +
+                        '<p class="desc">' + description[i] + '</p>' +
+                        '</div>' +
+                        '<a>');
+                }
+                $('#results').html(results.join(""));
+
             }
         });
     })
