3debe43724a4efb61c5fadead9dd160ef887f598
Nicholas Galarza <nicogala93@gmail.com>
2017-06-05 11:35:48 -0400

    Included animations for searching.

--- js/wiki.js
+++ js/wiki.js
@@ -1,39 +1,64 @@
 'use strict'
 $(document).ready(function() {
 
-    $('#submit').click(function() {
-        var keyword = $('#query').val(),
-            url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?";
+    $('.inputTab').hide();
 
-        $.ajax({
-            dataType: "jsonp",
-            type: "GET",
-            url: url,
-            async: true,
-            success: function(data) {
-                var searched = data[0],
-                    title = data[1],
-                    description = data[2],
-                    link = data[3],
-                    results = [];
+    $('#search').click(function(e) {
+        $('#search').toggle("slow");
+        $('#random').fadeOut(600);
+        $("#inputTab").append('<input id="query" type="text" name="Wikipedia"/>');
 
-                for (var i = 0; i < title.length; i++) {
-                    if (description[i] === "") {
-                        description[i] = "No description available for this article.";
+        $('#query').keypress(function(e) { // or document.
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == '13') {
+                var keyword = $('#query').val(),
+                    url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + keyword + "&format=json&callback=?";
+
+                $.ajax({
+                    dataType: "jsonp",
+                    type: "GET",
+                    url: url,
+                    async: true,
+                    success: function(data) {
+                        var searched = data[0],
+                            title = data[1],
+                            description = data[2],
+                            link = data[3],
+                            results = [];
+
+                        for (var i = 0; i < title.length; i++) {
+                            if (description[i] === "") {
+                                description[i] = "No description available for this article.";
+                            }
+
+                            if (title.length === 0) {
+                                results.push("No results were found for that search :(");
+                            }
+
+                            results.push('<a href="' + link[i] + '">' +
+                                '<div class="entry">' +
+                                '<h3 class="title">' + title[i] + '</h3>' +
+                                '<p class="desc">' + description[i] + '</p>' +
+                                '</div>' +
+                                '<a>');
+                        }
+                        $('#results').html(results.join(""));
+
                     }
-
-                    results.push('<a href="' + link[i] + '">' +
-                        '<div class="entry">' +
-                        '<h3 class="title">' + title[i] + '</h3>' +
-                        '<p class="desc">' + description[i] + '</p>' +
-                        '</div>' +
-                        '<a>');
-                }
-                $('#results').html(results.join(""));
-
+                }); // end of request.
             }
-        });
-    })
+        }) // end of event
+    });
+
+    $('#random').click(function(e) {
+        $('#random').toggle('highlight');
+        $('#search').fadeOut(600);
+
+
+    });
+
+
+
 
 
 });
